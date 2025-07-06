7fa9c7e66cbe5bdb7ab7af528c7b7ed79f71ffa2
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2025-07-06 12:28:39 -0400

    telescope.vim => telescope.lua

---
+++ debian/telescope.lua
@@ -0,0 +1,9 @@
+local function map(mode, lhs, rhs)
+    vim.keymap.set(mode, lhs, rhs, {noremap = true})
+end
+
+local builtin = require('telescope.builtin')
+map("n", "<leader>ff", builtin.find_files)
+map("n", "<leader>fg", builtin.live_grep)
+map("n", "<leader>fb", builtin.buffers)
+map("n", "<leader>fh", builtin.help_tags)
--- debian/telescope.vim
+++
@@ -1,6 +0,0 @@
-Alias tele Telescope
-
-nnoremap <leader>ff :Telescope find_files<cr>
-nnoremap <leader>fg :Telescope live_grep<cr>
-nnoremap <leader>fb :Telescope buffers<cr>
-nnoremap <leader>fh :Telescope help_tags<cr>
